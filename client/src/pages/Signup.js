b6ec5a81757f92e000d2063c6973127af52cab15
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-13 17:32:57 -0700

    token stored and retrived in header

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -32,7 +33,7 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState },
       });
-      console.log(data);
+      Auth.login(data.addUser.token);      
     } catch (e) {
       console.error(e);
     }
