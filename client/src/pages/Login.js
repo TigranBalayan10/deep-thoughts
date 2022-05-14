23f38937d68c403c275188e17f73f6cdc45c83e1
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-13 18:37:55 -0700

    conditionally render with token

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: "", password: "" });
-
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -26,10 +26,17 @@ const Login = (props) => {
       const { data } = await login({
         variables: { ...formState },
       });
+
       Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
   };
 
   return (
@@ -61,6 +68,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { useParams } from "react-router";
-import { useQuery } from "@apollo/client";
-import { QUERY_THOUGHT } from "../utils/queries";
-import ReactionList from "../components/ReactionList";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import ReactionList from '../components/ReactionList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -12,17 +14,18 @@ const SingleThought = (props) => {
   });
 
   const thought = data?.thought || {};
-  console.log(thought);
+
   if (loading) {
     return <div>Loading...</div>;
   }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
-          </span>{" "}
+          </span>{' '}
           thought on {thought.createdAt}
         </p>
         <div className="card-body">
